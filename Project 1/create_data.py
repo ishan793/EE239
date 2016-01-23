773b48979b50064381a69e8d63cbcff2499abcbd
ishan793 <ishan793@gmail.com>
2016-01-23 14:37:36 -0800

    with data

---
+++ Project 1/create_data.py
@@ -0,0 +1,67 @@
+import pickle
+import csv
+
+from sklearn.preprocessing import OneHotEncoder
+
+# function to convert a csv file to pickle
+def convert_pickle(filename, final_filename):
+	'''
+	Filename : input filename of csv filename
+	final_filename : o/p filename of .pickle file
+	'''
+	res = {'x':[],'y':[]}
+	with open(filename,'rb') as csvfile:
+		f = csv.reader(csvfile)
+		for line in f:
+			res['x'].append(line[:-1])
+			res['y'].append(line[-1])
+	
+	with open(final_filename,'wb') as f:
+		pickle.dump(res,f)
+
+
+def get_vec(d):
+	weeks = {'Monday':1,'Tuesday':2,'Wednesday':3,'Thursday':4,'Friday':5,'Saturday':6,'Sunday':7}
+	res = []
+	d[1] = weeks[d[1]]
+	d[3] = (d[3][10:])
+	d[4] = (d[4][5:])
+	res = [int(i) for i in d]
+	return res
+
+def get_num(x):
+	res = []
+	for i in x:
+		res.append(get_vec(i))
+	return res
+
+
+# function to convert a csv file to pickle
+def convert_network(filename,final_filename):
+	'''
+	Filename : input filename of csv filename
+	final_filename : o/p filename of .pickle file
+	'''
+	res = {'x':[],'y':[]}
+	with open(filename,'rb') as csvfile:
+		f = csv.reader(csvfile)
+		count = 0
+		for line in f:
+			if count != 0:
+				res['x'].append(line[:-2]+[line[-1]])
+				res['y'].append(line[-2])
+			count += 1
+
+	
+	
+	res['x'] = get_num(res['x'])
+	m = len(res['x'][0])-1
+	enc = OneHotEncoder(categorical_features = range(m),sparse = False)
+	enc.fit(res['x'])
+	res['x'] = enc.transform(res['x'])
+	
+	with open(final_filename,'wb') as f:
+		pickle.dump(res,f)
+
+convert_network('network_backup_dataset.csv','network.pickle')
+convert_pickle('housing_data.csv','housing_data.pickle')
