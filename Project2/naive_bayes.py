f0349d9eaa40862f9468ee49911525d54fb317b5
pulkit2311 <pulkitagrawal2311@gmail.com>
2016-02-22 15:18:27 -0800

    histogram

--- Project2/naive_bayes.py
+++ Project2/naive_bayes.py
@@ -5,6 +5,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
 
 with gzip.open("train_data.pkl.gz",'rb') as g:
   train_data=cPickle.load(g)
@@ -13,12 +15,18 @@ g.close()
 with gzip.open("test_data.pkl.gz",'rb') as g:
   test_data=cPickle.load(g)
 g.close() 
-
+print len(train_data[0])
 clf = GaussianNB()
 clf.fit(train_data[0],train_data[1])
+#y_score=clf.fit(train_data[0],train_data[1]).decision_function(test_data[0])
 predicted = clf.predict(test_data[0])
 label_test=test_data[1]
 print np.mean(predicted == label_test) 
 print confusion_matrix(label_test, predicted)
 print precision_score(label_test, predicted)
 print recall_score(label_test, predicted)
+fpr, tpr, thresholds = roc_curve(label_test, predicted)
+plt.plot(fpr,tpr)
+plt.show()
+
+
