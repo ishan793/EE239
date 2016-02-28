41ff1f69cd3513a56956080955527c1a38b865ac
ShubhamAgarwal12 <shubhamagarwaljhs@gmail.com>
2016-02-28 13:33:30 -0800

    Update project3B.m

--- Project1_804587205_204617837_004589213_204587029/project3B.m
+++ Project1_804587205_204617837_004589213_204587029/project3B.m
@@ -1,6 +1,15 @@
 clear
 clc
 A = importdata('E:\UCLA\Quarter2\BigData\Project3\ml-100k\ml-100k\u.data');
+
+%code for part A
+for i=1:100000
+    M(A(i,1),A(i,2))= A(i,3);
+end
+
+[A,Y,numIter,tElapsed,finalResidual] = wnmfrule(M,10);
+
+% code for part B
 k = length(A)/10;
 for i=1:10
     test(i,:,:) = get_matrix(A((i-1)*k+1:i*(k),:));
